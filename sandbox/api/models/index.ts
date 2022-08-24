1a31552a271e1f1ff31473d9b8393ab98b109a13
Alex Stelea <alex.s@rdx.works>
2022-08-24 11:20:57 +0200

    add sandbox environment

---
+++ sandbox/api/models/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccessRules';
+export * from './BadRequest';
+export * from './Component';
+export * from './ComponentBlueprint';
+export * from './Epoch';
+export * from './MethodRule';
+export * from './NonFungible';
+export * from './Nonce';
+export * from './OwnedResource';
+export * from './Receipt';
+export * from './Resource';
+export * from './ResourceMetadata';
+export * from './Signature';
+export * from './Transaction';
