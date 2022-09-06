46090ee8dfa190bd10190d2faf46b86d4193ca22
Alex Stelea <alex.s@rdx.works>
2022-09-06 16:47:26 +0200

    cleanup

--- lib/test-utils/helper.ts
+++ lib/test-utils/helper.ts
@@ -3,14 +3,12 @@ import { AccountAddressWalletResponse, requestType } from '../methods'
 
 const TestHelper = () => {
   const createAddresses = (numberOfAddresses: number) =>
-    new Array(numberOfAddresses)
-      .fill(null)
-      .map((_, index) => ({
-        address: `rdx${Buffer.from(crypto.randomUUID())
-          .toString('hex')
-          .slice(0, 32)}`,
-        label: `address-${index}`,
-      }))
+    new Array(numberOfAddresses).fill(null).map((_, index) => ({
+      address: `rdx${Buffer.from(crypto.randomUUID())
+        .toString('hex')
+        .slice(0, 32)}`,
+      label: `address-${index}`,
+    }))
   const createAccountAddressResponse = (
     numberOfAddresses: number
   ): AccountAddressWalletResponse => ({
