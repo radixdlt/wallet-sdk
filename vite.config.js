670e188524b574b1ade7e99f247818000c8dfc50
Alex Stelea <alex.s@rdx.works>
2022-10-13 13:03:08 +0200

    ci: update filename of esm build

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'lib/wallet-sdk.ts'),
       name: 'wallet-sdk',
-      fileName: (format) => `wallet-sdk.${format}.js`,
     },
   },
 })
