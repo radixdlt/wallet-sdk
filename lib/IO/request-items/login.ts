2cae930879c33c9c70eb1982405e467b4097899e
Alex Stelea <alex.s@rdx.works>
2023-01-18 16:25:03 +0100

    fix: support for login request without challenge

--- lib/IO/request-items/login.ts
+++ lib/IO/request-items/login.ts
@@ -34,13 +34,22 @@ export type Login = {
 }
 
 type NotAllowedKeys = Partial<{
-  usePersona: any
-  login: any
+  persona: any
+  loginWithoutChallenge: any
+  loginWithChallenge: any
 }>
 
-export const login =
-  (challenge?: string) =>
-  <I>(input: I extends NotAllowedKeys ? never : I) => ({
-    ...input,
-    login: { challenge },
-  })
+export const login = {
+  withoutChallenge:
+    () =>
+    <I>(input: I extends NotAllowedKeys ? never : I) => ({
+      ...input,
+      loginWithoutChallenge: {},
+    }),
+  withChallenge:
+    (challenge: string) =>
+    <I>(input: I extends NotAllowedKeys ? never : I) => ({
+      ...input,
+      loginWithChallenge: { challenge },
+    }),
+}
