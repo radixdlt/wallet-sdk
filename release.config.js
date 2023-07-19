6d7fca73d7208e02543b456aafd3f411c2e512be
Marek Karwacki <marek.karwacki@rdx.works>
2023-07-19 07:45:44 +0200

    ci: fix upload sbom on release

--- release.config.js
+++ release.config.js
@@ -8,6 +8,11 @@ module.exports = {
       prerelease: 'alpha',
     },
     {
+      name: 'fix-sbom',
+      channel: 'alpha',
+      prerelease: 'alpha',
+    },
+    {
       name: 'api-v2',
       channel: 'pre-alpha',
       prerelease: 'pre-alpha',
